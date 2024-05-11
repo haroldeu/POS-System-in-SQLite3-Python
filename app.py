6d845f48c207dbac9e9a766720730b9f4464d403
haroldeu <haroldeuu@gmail.com>
2024-05-11 13:12:20 +0800

    Created the static folder

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return render_template("index.html.j2")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
