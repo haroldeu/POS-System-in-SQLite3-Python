aa69004ac6d9ec6574a64af26422334832422bc4
haroldeu <haroldeuu@gmail.com>
2024-05-11 13:54:44 +0800

    Created and Connected a Database

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template, url_for
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html.j2")
+    # Connect to the Database
+    conn = sqlite3.connect('thesis.db')
+    c = conn.cursor()
+
+    # Execute the query
+    c.execute('SELECT * FROM products')
+
+    # Fetch all the rows
+    products = c.fetchall()
+
+    return render_template("index.html.j2", products=products)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
---
+++ sql.sql
@@ -0,0 +1,15 @@
+-- CREATE TABLE products (
+--     product_id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     product_name TEXT NOT NULL,
+--     product_image BLOB,
+--     product_price int NOT NULL,
+--     product_type TEXT NOT NULL,
+--     product_unit TEXT NOT NULL
+-- );
+
+-- INSERT INTO products (product_name, product_image, product_price, product_type, product_unit)
+-- VALUES ('Pechay', NULL, 55, 'Vegetable', 'kilo');
+
+
+
+SELECT * FROM products;
