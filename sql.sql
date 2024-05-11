93f761242d985c603e9f670c2b53ea609f75b123
haroldeu <haroldeuu@gmail.com>
2024-05-11 16:07:57 +0800

    Added records manually on the database

--- sql.sql
+++ sql.sql
@@ -1,15 +1,32 @@
+-- !!!!!!!!!!!!!!!! WAGGGGGG !!!!!!!!!!!!!!!
+-- DROP TABLE PRODUCTS;
+
+
 -- CREATE TABLE products (
 --     product_id INTEGER PRIMARY KEY AUTOINCREMENT,
---     product_name TEXT NOT NULL,
 --     product_image BLOB,
+--     product_name TEXT NOT NULL,
 --     product_price int NOT NULL,
 --     product_type TEXT NOT NULL,
 --     product_unit TEXT NOT NULL
 -- );
 
--- INSERT INTO products (product_name, product_image, product_price, product_type, product_unit)
--- VALUES ('Apple', NULL, 30, 'Fruit', 'pcs');
+INSERT INTO products (product_image, product_name, product_price, product_type, product_unit)
+VALUES (NULL, 'Pechay', 55, 'Vegetable', 'kilo');
 
 
 
 SELECT * FROM products;
+
+
+
+
+
+
+
+
+
+
+
+
+
